61e663dd01fccb564b4a7564372b824d90e48b52
chrisd0314 <chihiro603@gmail.com>
2025-05-29 15:12:40 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -3,9 +3,8 @@ const btn = document.getElementById('btn');
 
 // HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click',() => {
-    //新しくh2要素を作成し、定数に代入する
-    const h2 = document.createElement(h2);
-    // 作成したh2要素にテキストを追加する
-    h2.textContent = 'ボタンをクリックしました';
-
+    // textというidを持つHTML要素を取得し、定数に代入する
+    const text = document.getElementById('text');
+    // 定数textに、新しいテキストを代入する
+    text.textContent = 'ボタンをクリックしました';
 })
